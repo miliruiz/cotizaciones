953a4fa62658ca5885a74e5f178a09293b959e2c
Milagros Abril Ruiz <abriferrin11@gmail.com>
2022-05-24 01:25:25 -0300

    agregue libreria

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+let valores = {
+    dolar: 200.05,
+    euro: 231,
+    chileno: 0.13,
+    reales: 22.79,
+    libras: 144.48,
+}
+
+let { dolar, euro, chileno, reales, libras } = valores
+
+for (let i = 0; i < valores.length; i++) {
+    console.log(valores[i].nombre);
+    console.log(valoress[i].valor);
+}
+
 cotizaciones = [
     dolar = 200.05,
     euro = 231,
@@ -6,11 +22,6 @@ cotizaciones = [
     libras = 144.48,
 ]
 
-for (let i = 0; i < cotizaciones.length; i++) {
-    console.log(cotizaciones[i].nombre);
-    console.log(cotizaciones[i].valor);
-
-}
 
 
 function convertir() {
@@ -19,35 +30,37 @@ function convertir() {
 
     if (document.getElementById("dolar").checked) {
         resultado = conversion / dolar;
-        alert("El cambio de pesos ARG a dolares es: $" + resultado.toFixed(2));
-        console.log()
+        swal("El cambio de pesos ARG a dolar es: $", resultado.toFixed(2));
     }
     else if (document.getElementById("euro").checked) {
         resultado = conversion / euro;
-        alert("El cambio de pesos ARG a euros es: €" + resultado.toFixed(2));
+        swal("El cambio de pesos ARG a euro es: €", resultado.toFixed(2));
     }
     else if (document.getElementById("chileno").checked) {
         resultado = conversion / chileno;
-        alert("El cambio de pesos ARG a pesos chilenos es: $" + resultado.toFixed(2));
+        swal("El cambio de pesos ARG a peso chileno es es: $", resultado.toFixed(2));
     }
     else if (document.getElementById("real").checked) {
         resultado = conversion / reales;
-        alert("El cambio de pesos ARG a euros es: R$" + resultado.toFixed(2));
+        swal("El cambio de pesos ARG a reales es: R$", resultado.toFixed(2));
     }
     else if (document.getElementById("libras").checked) {
         resultado = conversion / libras;
-        alert("El cambio de pesos ARG a libras esterlinas es: GBP " + resultado.toFixed(2));
+        swal("El cambio de pesos ARG a libras esterlinas es: £", resultado.toFixed(2));
     }
     else {
-        alert("Tienes que completar todos los requerimientos");
+        swal("Error", "Tienes que completar todos los requerimientos");
     }
 }
 
 
-function pulsar(e) {
-    if (e.keyCode === 13 ) {
-        
-    }
+const input = document.getElementById('valor');
+const log = document.getElementById('cotizador');
+
+input.addEventListener('change', updateValue);
+
+function updateValue(e) {
+    log.textContent = e.target.value;
 }
 
 
